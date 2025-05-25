ba8a12fe8102aa9e5afd53cb05a5126efa9b9e99
Vladislav Dimitrov <vladislav.d.dimit@gmail.com>
2025-05-25 17:29:45 +0300

    Add more parsingHelpers

--- src/main/java/com/nbu/pressofficeapp/utils/ParsingHelpers.java
+++ src/main/java/com/nbu/pressofficeapp/utils/ParsingHelpers.java
@@ -1,14 +1,44 @@
 package com.nbu.pressofficeapp.utils;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class ParsingHelpers {
+    private static final String INVALID_VALUE_TYPE = "%s is an incompatible value type!";
     public static <E extends Enum<E>> E tryParseEnum(String valueToParse, Class<E> type, String errorMessage) {
         try {
             return Enum.valueOf(type, valueToParse.toUpperCase());
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException(String.format(errorMessage, valueToParse));
         }
+    }
+
+    public static Integer tryParseInteger(String valueToParse){
+        try {
+            return Integer.parseInt(valueToParse);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException( String.format(INVALID_VALUE_TYPE, valueToParse));
+        }
+    }
+
+    public static Long tryParseLong(String valueToParse){
+        try {
+            return Long.parseLong(valueToParse);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException( String.format(INVALID_VALUE_TYPE, valueToParse));
+        }
+    }
+
+    public static Double tryParseDouble(String valueToParse){
+        try{
+            return Double.parseDouble(valueToParse);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException( String.format(INVALID_VALUE_TYPE, valueToParse));
+        }
+    }
+
+    public static BigDecimal tryParseBigDecimal(String valueToParse){
+            return BigDecimal.valueOf(tryParseDouble(valueToParse));
     }
 
     public static <E> void checkIfCollectionEmpty(List<E> collection, String errorMessage){
